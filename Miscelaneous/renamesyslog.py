b3b8cadebba6b082e8feefc81a2f3d13c6466933
wilmeist3r <56273720+wilmeist3r@users.noreply.github.com>
2021-01-24 20:48:11 -0500

    Update renamesyslog.py
    Updated for universality.

--- Miscelaneous/renamesyslog.py
+++ Miscelaneous/renamesyslog.py
@@ -9,26 +9,19 @@ def main():
 
 
     # Creating lists for the extraction of strings corresponding to sequence numbers and datestamps of the filenames
-    spltXD = [str(i).split("xxxxxx_")[1:] for i in dirElements]     # Modify filename prefix as needed
-
-    spltX = [str(i).split("_")[1:] for i in spltXD]
-    spltX = [str(i).split("'")[0] for i in spltX]
-    spltX = [str(i).split('["')[1:] for i in spltX]
-
-    spltD = [str(i).split("_")[0] for i in spltXD]
-    spltD = [str(i).split("['")[1:] for i in spltD]
+    spltSD = [str(i).split("_")[1:] for i in dirElements]     # Modify filename prefix as needed
 
     # Final managed lists containing every file's sequence number and datestamp, respectively
-    seqNum = [''.join(map(str, i)) for i in spltX]
-    dateNum = [''.join(map(str, i)) for i in spltD]
+    spltS = [spltSD[spltSD.index(i)][1] for i in spltSD]
+    spltD = [spltSD[spltSD.index(i)][0] for i in spltSD]
 
     # Empty list to contain the full modified filenames
     fullName = []
 
 
-    for seq in seqNum:
-        for date in dateNum:
-            fullName.append("xxxxxx_yyyyyy_zzzzzz" + seq + "_" + date) # Populating fullName list. Change depending upon criteria
+    for seq in spltX:
+        for date in spltD:
+            fullName.append("CI4GERIC_ASMAT_000000" + seq + "_" + date) # Populating fullName list. Change depending upon criteria
 
     for file, name in zip(dirElements, fullName):
         os.rename(os.path.join(srcPath, file), os.path.join(dstPath, name)) # Renaming files
